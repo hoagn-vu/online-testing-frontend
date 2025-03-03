af58a166ef6daf6e0fd31ce768b9f706a9f6f26a
phuonglinh81 <akagamishirayuki88@gmail.com>
2025-03-03 12:22:12 +0700

    edit question card

--- src/components/QuestionCard/QuestionCard.js
+++ src/components/QuestionCard/QuestionCard.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useState } from 'react';
 import { FlagIcon } from "lucide-react";
 import './QuestionCard.css'
 
-const QuestionCard = ({ question, options, questionNumber }) => {
+const QuestionCard = ({ question, options, questionNumber, allowMultiple }) => {
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+    const handleOptionClick = (option) => {
+        if (allowMultiple) {
+            setSelectedOptions(prev => 
+                prev.includes(option) 
+                    ? prev.filter(item => item !== option) 
+                    : [...prev, option]
+            );
+        } else {
+            setSelectedOptions([option]);
+        }
+    };
+
   return (
-    <div className=" p-4 rounded-xl shadow-md d-flex">
-      <div>
-        <p className="text-lg font-bold mb-0">Câu {questionNumber}: <span>{question}</span> </p> 
-        <ul className="">
-          {options.map((option, index) => (
-            <li key={index} className="flex items-center">
-              <input type="radio" name={`question-${questionNumber}`} id={`option-${index}`} />
-              <label htmlFor={`option-${index}`} className="ml-2 cursor-pointer">
-                {option}
-              </label>
-            </li>
-          ))}
-        </ul>
-      </div>
-      {/* Nút gắn cờ */}
-      <button className="absolute top-4 right-4 p-2 rounded-lg border shadow-md hover:bg-gray-100 btn-question-card">
-        <FlagIcon size={16} />
-      </button>
-    </div>
+    <div className="question-card">
+            <div className="header-question-card">
+                <h2>Question {questionNumber}</h2>
+                <button><i className="fa-solid fa-flag"></i></button>
+            </div>
+            <p>{question}</p>
+            <ul className="options-list">
+                {options.map((option, index) => (
+                    <li key={index} className={`option ${selectedOptions.includes(option) ? 'selected' : ''}`} 
+                        onClick={() => handleOptionClick(option)}>
+                        {option}
+                    </li>
+                ))}
+            </ul>
+        </div>
   );
 };
 
