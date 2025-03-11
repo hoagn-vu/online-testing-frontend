e9bd970c811bdf3e03bc751e2326e476207440aa
hoagn-vu <hoangvu0363@gmail.com>
2025-03-11 08:45:30 +0700

    update prop types validation

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const Breadcrumbs = ({ currentPage }) => {
   return (
@@ -12,6 +13,10 @@ const Breadcrumbs = ({ currentPage }) => {
       <span className="breadcrumb-current">{currentPage}</span>
     </nav>
   );
+};
+
+Breadcrumbs.propTypes = {
+  currentPage: PropTypes.string.isRequired,
 };
 
 export default Breadcrumbs;
