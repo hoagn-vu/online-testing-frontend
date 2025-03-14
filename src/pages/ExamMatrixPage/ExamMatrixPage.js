de1dd77529cea182cd52b3542cd7076d607fd18d
phuonglinh81 <akagamishirayuki88@gmail.com>
2025-03-15 01:41:43 +0700

    Update form edit exam matrix

--- src/pages/ExamMatrixPage/ExamMatrixPage.js
+++ src/pages/ExamMatrixPage/ExamMatrixPage.js
@@ -15,7 +15,7 @@ const listQuestionBank = [
         "id": "matrix_001",
         "questionBankId": "812004",
         "matrixName": "Đề thi Toán học kỳ 1",
-        "matrixStatus": "Đã duyệt",
+        "matrixStatus": "Ative",
         "totalGeneratedExams": 10,
         "subjectId": "math_101",
         "matrixTags": [
@@ -46,13 +46,72 @@ const listQuestionBank = [
       
 ];
 
+const colourOptions = [
+	{ value: 'red', label: 'Tư tưởng Hồ Chí Minh Tư tưởng Hồ Chí Minh Tư ' },
+	{ value: 'blue', label: 'Blue' },
+	{ value: 'green', label: 'Green' },
+	{ value: 'yellow', label: 'Yellow' },
+	{ value: 'purple', label: 'Purple' },
+];
+
 const ExamMatrixPage = () => {
+		const [showForm, setShowForm] = useState(false);
+		const [editingAccount, setEditingAccount] = useState(null);
+		const inputRef = useRef(null);
     const [rows, setRows] = useState(Object.values(listQuestionBank).flat());
     const handleStatusChange = (id, newStatus) => {
 			setRows(
 				rows.map((row) => (row.id === id ? { ...row, status: newStatus } : row))
 			);
 		};
+
+		const [formData, setFormData] = useState({
+				matrixName: "",
+				subjectId: "",
+				questionBankId: "",
+				matrixStatus: "active",
+		});
+
+		const handleEdit = (account) => {
+			setFormData({
+				matrixName: account.matrixName,
+				subjectId: account.subjectId,
+				questionBankId: account.questionBankId,
+				matrixStatus: account.matrixStatus,
+			});
+			setEditingAccount(account);
+			setShowForm(true);
+		};
+
+		const handleSubmit = (e) => {
+			e.preventDefault();
+			console.log("Dữ liệu thêm mới:", formData);
+			setShowForm(false);
+		};
+
+		const handleDelete = (id) => {
+				Swal.fire({
+					title: "Bạn có chắc chắn xóa?",
+					text: "Bạn sẽ không thể hoàn tác hành động này!",
+					icon: "warning",
+					showCancelButton: true,
+					confirmButtonColor: "#3085d6",
+					cancelButtonColor: "#d33",
+					confirmButtonText: "Xóa",
+					cancelButtonText: "Hủy",
+				}).then((result) => {
+					if (result.isConfirmed) {
+					console.log("Xóa tài khoản có ID:", id);
+			
+					Swal.fire({
+						title: "Đã xóa!",
+						text: "Tài khoản đã bị xóa.",
+						icon: "success",
+					});
+					setRows(rows.filter((row) => row.id !== id));
+					}
+				});
+			};
     const columns = [
         { field: "stt", headerName: "#", width: 15, align: "center", headerAlign: "center" },
         { 
@@ -71,7 +130,7 @@ const ExamMatrixPage = () => {
             width: 1090, flex: 0.1, 
         },
         {
-            field: "accountStatus",
+            field: "matrixStatus",
             headerName: "Trạng thái",
             width: 160,
             renderCell: (params) => (
@@ -158,6 +217,93 @@ const ExamMatrixPage = () => {
                 />
                 </Paper>
             </div>
+						{/* Form thêm tài khoản */}
+						{showForm && (
+							<div className="form-overlay">
+								<Box
+									component="form"
+									sx={{
+										width: "600px",
+										backgroundColor: "white",
+										p: 2,
+										borderRadius: "8px",
+										boxShadow: 3,
+										mx: "auto",
+									}}
+									onSubmit={handleSubmit}
+								>
+									<p className="text-align fw-bold">
+										{"Chỉnh sửa ma trận đề thi" }
+									</p>
+									<Grid container spacing={2}>
+										<Grid item xs={6}>
+											<TextField
+												fullWidth
+												label="Ma trận đề thi"
+												required
+												value={formData.matrixName}
+												inputRef={inputRef}
+												onChange={(e) =>
+													setFormData({ ...formData, studentId: e.target.value })
+												}
+												sx={{
+													"& .MuiInputBase-input": {
+														fontSize: "14px",
+														paddingBottom: "11px",
+													},
+													"& .MuiInputLabel-root": { fontSize: "14px" }, // Giảm cỡ chữ label
+												}}
+											/>
+										</Grid>
+										<Grid item xs={6}>
+											<TextField
+												fullWidth
+												select
+												label="Trạng thái"
+												required
+												value={formData.matrixStatus}
+												onChange={(e) =>
+													setFormData({ ...formData, status: e.target.value })
+												}
+												sx={{
+													"& .MuiInputBase-input": {
+														fontSize: "14px",
+														paddingBottom: "11px",
+													},
+													"& .MuiInputLabel-root": { fontSize: "14px" }, // Giảm cỡ chữ label
+												}}
+											>
+												<MenuItem value="active">Active</MenuItem>
+												<MenuItem value="disabled">Disabled</MenuItem>
+											</TextField>
+										</Grid>
+									</Grid>		
+									{/* Buttons */}
+									<Grid container spacing={2} sx={{ mt: 2 }}>
+										<Grid item xs={6}>
+											<Button
+												type="submit"
+												variant="contained"
+												color="primary"
+												fullWidth
+											>
+												{editingAccount ? "Cập nhật" : "Lưu"}
+											</Button>
+										</Grid>
+										<Grid item xs={6}>
+											<Button
+												variant="outlined"
+												color="secondary"
+												fullWidth
+												onClick={() => setShowForm(false)}
+											>
+												Hủy
+											</Button>
+										</Grid>
+									</Grid>
+								</Box>
+							</div>
+						)}
         </div>
     );
 };
