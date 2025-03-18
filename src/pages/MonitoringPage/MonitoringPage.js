f1eefdff6b9a461e1c8c2a3f1d1135931e049cc8
phuonglinh81 <akagamishirayuki88@gmail.com>
2025-03-18 23:40:52 +0700

    update monitor table

--- src/pages/MonitoringPage/MonitoringPage.js
+++ src/pages/MonitoringPage/MonitoringPage.js
@@ -21,10 +21,19 @@ const MonitoringPage = () => {
 			userCode: "BIT220172",
 			fullName: "Ngô Đức Thuận",
 			gender: "Nam",
+			dateOfBirth: "21/05/2004 21/05/2004",
+			examId: "EXAM124"
+		},
+		{
+			candidateId: "CAND002",
+			userCode: "BIT220172",
+			fullName: "Ngô Đức Thuận",
+			gender: "Nam",
 			dateOfBirth: "21/05/2004",
 			examId: "EXAM124"
 		},
 	];
+	
 
   const [selectedExam, setSelectedExam] = useState(null);
   const [showModal, setShowModal] = useState(false);
@@ -51,29 +60,29 @@ const processData = (data) => {
 	
   return (
     <div className="home-super-page">
-			<Link to="/supervisor/home" style={{ display: "flex", alignItems: "center", textDecoration: "none" }}>
+			<Link className="mt-0" to="/supervisor/home" style={{ display: "flex", alignItems: "center", textDecoration: "none" }}>
 				<ChevronLeftIcon /> Quay về
 			</Link>
-			<div className="mb-4">
-				<h4 className="d-flex align-items-center justify-content-center">Giám sát kỳ thi: </h4> 
-				<h5 className="d-flex align-items-center justify-content-center">Ca thi: </h5>
-				<h5 className="d-flex align-items-center justify-content-center">Phòng thi: </h5>
+			<div className="mb-3 mt-1">
+				<h5 className="d-flex align-items-center justify-content-center">Giám sát kỳ thi: </h5> 
+				<h6 className="d-flex align-items-center justify-content-center">Ca thi: </h6>
+				<h6 className="d-flex align-items-center justify-content-center">Phòng thi: </h6>
 			</div>
 			<div>
-				<table className="table table-hover table-bordered custom-table">
-					<thead className="table-dark">
+				<table className="table table-hover table-bordered custom-table monitor-tbl">
+					<thead className="custom-header">
 						<tr>
 							<th>#</th>
-							<th>Mã sinh viên</th>
+							<th style={{width: "100px"}}>Mã sinh viên</th>
 							<th className="text-wrap" style={{ maxWidth: "100px" }}>Họ và tên đệm</th>
-							<th>Tên</th>
+							<th style={{minWidth: "70px"}}>Tên</th>
 							<th>Trạng thái</th>
 							<th>Bắt đầu</th>
-							<th>Kết thúc</th>
 							<th>Tiến độ</th>
 							<th>Nộp bài</th>
 							<th>Điểm số</th>
 							<th>Cảnh cáo</th>
+							<th>Thao tác</th>
 						</tr>
 					</thead>
 					<tbody>
@@ -85,7 +94,7 @@ const processData = (data) => {
 								<td>{row.firstName}</td>
 								<td>{row.gender}</td>
 								<td>{row.dateOfBirth}</td>
-								<td>{row.dateOfBirth}</td>
+								<td>0/10</td>
 								<td>{row.dateOfBirth}</td>
 								<td></td>
 								<td>{row.dateOfBirth}</td>
