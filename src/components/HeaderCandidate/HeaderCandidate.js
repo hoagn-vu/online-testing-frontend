7992598745090c3f115ac8292321aeccb4fb0679
phuonglinh81 <akagamishirayuki88@gmail.com>
2025-04-06 23:53:22 +0700

    update logout dropdown

--- src/components/HeaderCandidate/HeaderCandidate.js
+++ src/components/HeaderCandidate/HeaderCandidate.js
@@ -44,23 +44,28 @@ const HeaderCandidate = ({ avatarUrl, logoUrl }) => {
             Lịch sử thi
           </Link>
         </nav>
-        <img src={avatarUrl} alt="Avatar" className="avatar" />
-        <span className="username">{user?.username}</span>
+        <img src={avatarUrl} alt="Avatar" className="avatar" onClick={() => setIsOpen(!isOpen)} style={{ cursor: "pointer" }}/>
+        <span className="username" onClick={() => setIsOpen(!isOpen)} style={{ cursor: "pointer" }}>{user?.username} </span>
 
         {/* Nút mở dropdown */}
         <button className="dropdown-btn" onClick={() => setIsOpen(!isOpen)}>
           <FaChevronDown />
         </button>
-
+        
         {/* Dropdown menu */}
         {isOpen && (
-          <ul className="dropdown-menu show position-absolute end-0 mt-2">
-            <li>
-              <a className="dropdown-item" onClick={handleLogout}>
-                Đăng xuất
-              </a>
-            </li>
-          </ul>
+          <ul className="dropdown-menu show position-absolute end-0 mt-2 custom-dropdown">
+          <li>
+            <a
+              className="dropdown-item d-flex justify-content-between align-items-center"
+              onClick={handleLogout}
+              style={{ gap: "8px" }}
+            >
+              <span>Đăng xuất</span>
+              <i className="fa-solid fa-right-from-bracket" aria-hidden="true"></i>
+            </a>
+          </li>
+        </ul>
         )}
       </div>
     </div>
