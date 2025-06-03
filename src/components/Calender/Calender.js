dfae456faffc199a02f7338ca11ab209c150ae09
phuonglinh81 <akagamishirayuki88@gmail.com>
2025-06-03 21:14:32 +0700

    create component calendar

---
+++ src/components/Calender/Calender.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+function MyCalendar() {
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <div style={{ maxWidth: '350px', margin: 'auto', height: '1000px' }}>
+      <Calendar
+        onChange={setDate}
+        value={date}
+        locale="vi-VN"
+      />
+      {/* <p>Ngày bạn chọn: {date.toLocaleDateString('vi-VN')}</p> */}
+    </div>
+  );
+}
+
+export default MyCalendar;
