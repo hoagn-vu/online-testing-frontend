8570c8a5b64657daeef92f344ebc0645344a7f50
hoagn-vu <hoangvu0363@gmail.com>
2025-09-01 00:00:52 +0700

    Update apiService.js

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_BASE_URL = process.env.REACT_APP_BASE_URL;
-const API_BASE_URL = "";
+const API_BASE_URL = process.env.REACT_APP_BASE_URL;
+// const API_BASE_URL = "";
 
 const getToken = () => {
   return localStorage.getItem("accessToken");
